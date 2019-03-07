bf6615c2c5bb56c6af081ceb16d88e94579196ed
Thomas Lowry <minilao94@yahoo.com>
2019-03-07 14:12:10 -0700

    moved notifications to storageprovider

--- App.js
+++ App.js
@@ -1,44 +1,11 @@
 import React, { Component } from 'react';
 import AppNavigator from './navigation/AppNavigator';
 import StorageProvider from './storage/StorageProvider';
-import OneSignal from 'react-native-onesignal';
-import { handleReceivedNotification } from './storage/service';
 
-export default class App extends Component {
-
-    state = {
-        notification: {},
-    };
-
-    constructor(properties) {
-        super(properties);
-        OneSignal.init("d8ca736c-86df-4151-9df8-2fbfecf81436");
-
-        OneSignal.addEventListener('received', handleReceivedNotification);
-        OneSignal.addEventListener('opened', this.onOpened);
-        OneSignal.addEventListener('ids', this.onIds);
-    }
-
-    componentWillUnmount() {
-        OneSignal.removeEventListener('received', handleReceivedNotification);
-        OneSignal.removeEventListener('opened', this.onOpened);
-        OneSignal.removeEventListener('ids', this.onIds);
-        clearInterval(this.interval);
-    }
-
-    onReceived = notification => this.setState({ notification });
-
-    onOpened(openResult) {}
-
-    onIds(device) {
-        console.log('Device info: ', device);
-    }
-
-    render() {
-        return (
-            <StorageProvider newNotification={this.state.notification} >
-                <AppNavigator />
-            </StorageProvider>
-        );
-    }
+export default function App() {
+    return (
+        <StorageProvider newNotification={this.state.notification} >
+            <AppNavigator />
+        </StorageProvider>
+    );
 }
--- storage/StorageProvider.js
+++ storage/StorageProvider.js
@@ -3,6 +3,8 @@ import React, {
     createContext,
 } from 'react';
 
+import OneSignal from 'react-native-onesignal';
+
 import {
     log,
     getItem,
@@ -57,24 +59,32 @@ export default class StorageProvider extends Component {
         deleteNotification() { },
     };
 
-    componentDidUpdate = async ({ newNotification: oldNotification }) => {
-        const {
-            props: {
-                newNotification,
-            },
-        } = this;
-        if (newNotification !== oldNotification) {
-            try {
-                const notifications = await handleReceivedNotification(newNotification);
+    /**
+     * I'm not sure I know what's wrong actually,
+     * This component wraps the entire app and so it is always mounted.
+     * When it mounts, it gets all the notifications from asyncstorage.
+     * Then it handles receiving a new notification when one comes in while the app is open.
+     * 
+     * What old notification?
+     */
 
-                if (notifications) this.setState({ notifications });
-            } catch (err) {
-                console.error(err);
-            }
-        }
+    // this lifecycle hook receive prevProps and prevState as arguments. 
+    // oldnotification is the one from prvious props
+
+    componentWillUnmount() {
+        OneSignal.removeEventListener('received', this.receiveNotification);
+        OneSignal.removeEventListener('opened', this.onOpened);
+        OneSignal.removeEventListener('ids', this.onIds);
+        clearInterval(this.interval);
     }
 
     componentDidMount = async () => {
+
+        OneSignal.init("d8ca736c-86df-4151-9df8-2fbfecf81436");
+
+        OneSignal.addEventListener('received', this.receiveNotification);
+        OneSignal.addEventListener('opened', this.onOpened);
+        OneSignal.addEventListener('ids', this.onIds);
 
         await refetchSessionsEachDay();
 
@@ -102,7 +112,7 @@ export default class StorageProvider extends Component {
             notifications,
             addToSchedule: this.addToSchedule,
             removeFromSchedule: this.removeFromSchedule,
-            deleteNotification:this.deleteNotification,
+            deleteNotification: this.deleteNotification,
             submitReview,
         });
     }
@@ -155,6 +165,22 @@ export default class StorageProvider extends Component {
         } catch (err) {
             console.error(err);
         }
+    }
+
+    receiveNotification = async notification => {
+        try {
+            const notifications = await handleReceivedNotification(notification);
+
+            if (notifications) this.setState({ notifications });
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    onOpened(openResult) { }
+
+    onIds(device) {
+        console.log('Device info: ', device);
     }
 
     render = () => (
